17ef4756d2be1c2abbecaa8798e7491f8d3d3f0a
Daniel <danielmaks400@gmail.com>
2025-10-21 12:50:00 +0200

    10/21 changes entity + database

---
+++ src/main/java/via/dk/dataserver/entity/Users.java
@@ -0,0 +1,31 @@
+package via.dk.dataserver.entity;
+
+import io.grpc.netty.shaded.io.netty.util.internal.StringUtil;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="users", schema ="sep3" )
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(name ="username",nullable = false)
+    String username;
+    @Column(name="password", nullable = false)
+    String password;
+    @Column(name ="email", nullable = false)
+    String email;
+    public void setUsername(String username) {
+        //instead of StringUtility we used StringUtil if some errors later that may be the case
+
+        if(StringUtil.isNullOrEmpty(username)){
+            throw new IllegalArgumentException("username characters  cannot be null or empty");
+        } else if (username.length() < 4 || username.length()>30) {
+            throw new IllegalArgumentException("username characters must be less 30 characters long");
+        }
+        this.username = username;
+    }
+
+
+
+}
--- src/main/java/via/dk/dataserver/persistence/init/Sep3_Database.sql
+++ src/main/java/via/dk/dataserver/persistence/init/Sep3_Database.sql
@@ -1,14 +1,14 @@
 Create SCHEMA IF NOT EXISTS sep3;
 set SCHEMA 'sep3';
 drop schema sep3 cascade;
-Create table if not exists "user"(
+Create table if not exists users(
     id serial PRIMARY KEY ,
     username varchar (30),
     password varchar (30),
     email text
     );
-drop table "user";
+drop table users;
 
-INSERT INTO "user"(username, password, email) VALUES ('example','example123','email@example.com');
+INSERT INTO users(username, password, email) VALUES ('example','example123','email@example.com');
 
 
