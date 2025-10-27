825adb3452f5864bdf5258be1f2c0203dfb7c4d1
Daniel <danielmaks400@gmail.com>
2025-10-27 11:15:14 +0100

    sepserver

--- src/main/java/dk/via/dataserver/Sep3JavaDataServerApplication.java
+++ src/main/java/dk/via/dataserver/Sep3JavaDataServerApplication.java
@@ -1,23 +1,31 @@
 package dk.via.dataserver;
 
+import dk.via.dataserver.networking.MainHandler;
+import dk.via.dataserver.networking.SepServer;
+import dk.via.dataserver.startup.GlobalContext;
+import io.grpc.Server;
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import dk.via.dataserver.entity.User;
 import dk.via.dataserver.repository.UserRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableJpaRepositories("dk.via.dataserver.repository")
+@EntityScan("dk.via.dataserver.entity")
+@SpringBootApplication(scanBasePackages = "dk.via.dataserver")
 public class Sep3JavaDataServerApplication
 {
 
   public static void main(String[] args)
   {
-    ApplicationContext context = SpringApplication.run(Sep3JavaDataServerApplication.class, args);
-    UserRepository userRepository = context.getBean(UserRepository.class);
-    User user = new User();
-    user.setUsername("Mario");
-    user.setPassword("asd123");
-    user.setEmail("mario@mvp.com");
-    userRepository.save(user);
+    GlobalContext.setContext(SpringApplication.run(Sep3JavaDataServerApplication.class,args));
+    //ApplicationContext context = SpringApplication.run(Sep3JavaDataServerApplication.class, args);
+    UserRepository userRepository = GlobalContext.getBean(UserRepository.class);
+    SepServer server = GlobalContext.getContext().getBean(SepServer.class);
+    server.start();
+    System.out.println("Server started");
   }
 }
--- src/main/java/dk/via/dataserver/networking/SepServer.java
+++ src/main/java/dk/via/dataserver/networking/SepServer.java
@@ -2,13 +2,15 @@ package dk.via.dataserver.networking;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
+@Component
 public class SepServer {
     private static final int PORT = 7991;
     private final MainHandler handler;
     private Server grpcServer;
+
 
     public SepServer(MainHandler handler) {
         this.handler = handler;
