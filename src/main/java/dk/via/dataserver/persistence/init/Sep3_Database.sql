05160d27a8385e85aa21dee24a358b870c868f54
infrector <infrector@gmail.com>
2025-12-05 12:37:56 +0100

    Removed GameResult dbs entity

--- src/main/java/dk/via/dataserver/persistence/init/Sep3_Database.sql
+++ src/main/java/dk/via/dataserver/persistence/init/Sep3_Database.sql
@@ -5,21 +5,13 @@ DROP SCHEMA sep3 CASCADE;
 
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY ,
-    username VARCHAR (30),
-    password VARCHAR (50),
-    email TEXT,
-    score DOUBLE PRECISION
+    username VARCHAR (30) NOT NULL UNIQUE ,
+    password VARCHAR (50) NOT NULL ,
+    email TEXT NOT NULL UNIQUE ,
+    points DOUBLE PRECISION
     );
 
-CREATE TABLE game_results (
-    id SERIAL PRIMARY KEY,
-    game_id INTEGER NOT NULL UNIQUE,
-    winner_id INTEGER NOT NULL,
-    looser_id INTEGER NOT NULL,
-    is_draw BOOLEAN NOT NULL
-);
-
-drop table game_results;
+drop table users;
 
 INSERT INTO users(username, password, email) VALUES ('example','example123','email@example.com');
 
